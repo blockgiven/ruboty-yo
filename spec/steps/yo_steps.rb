0a7ad684786e7cbb25572669ced330c10c6192a1
block_given? <block_given@outlook.com>
2014-11-01 03:49:52 +0900

    fix webmock spec

--- spec/steps/yo_steps.rb
+++ spec/steps/yo_steps.rb
@@ -20,12 +20,12 @@ end
 
 step 'Rubotyは:nameにyoと伝えた' do |name|
   expect(@yo_users).to include name
-  expect(WebMock).to have_requested(:post, 'api.justyo.co/yo/').with {|req| req.body == "username=#{name}" }
+  expect(WebMock).to have_requested(:post, 'https://api.justyo.co/yo/').with {|req| req.body.include? "username=#{name}" }
 end
 
 step 'Rubotyは:nameにyoと伝えられなかった' do |name|
   expect(@yo_users).not_to include name
-  expect(WebMock).not_to have_requested(:post, 'api.justyo.co/yo/').with {|req| req.body == "username=#{name}" }
+  expect(WebMock).to have_requested(:post, 'https://api.justyo.co/yo/').with {|req| req.body.include? "username=#{name}" }
 end
 
 step 'Rubotyは:bodyと言った' do |body|
